f4d86f8500b9ca5757dd3a957167afd03b593ac7
TaranAleksandr <108215188+TaranAleksandr@users.noreply.github.com>
2022-12-06 20:16:05 +0300

    Отрисовка карточек ресторанов и товаров
    Добавлена автоматическая отрисовка карточек ресторанов и товаров, в зависимости от ресторана

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,66 @@
-const restaurant = 'food-band'
+const cardsMenu = document.querySelector('.cards-menu')
 
-const renderItem = (data) => {
-  console.log(data);
+const changeTitle = (restaurant) => {
+  const restaurantTitle = document.querySelector('.restaurant-title')
+  const rating = document.querySelector('.rating')
+  const price = document.querySelector('.price')
+  const category = document.querySelector('.category')
+
+  restaurantTitle.textContent = restaurant.name
+  rating.textContent = restaurant.stars
+  price.textContent = restaurant.price
+  category.textContent = restaurant.kitchen
 }
 
-fetch(`./db/${restaurant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderItem(data);
+
+
+const renderItem = (data) => {
+  data.forEach((item) => {
+    const { name, description, price, image, id } = item
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    card.innerHTML = `
+              <img
+                src="${image}"
+                alt="${name}"
+                class="card-image"
+              />
+              <div class="card-text">
+                <div class="card-heading">
+                  <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <div class="card-info">
+                  <div class="ingredients">
+                    ${description}
+                  </div>
+                </div>
+                <div class="card-buttons">
+                  <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                  </button>
+                  <strong class="card-price-bold">${price} ₽</strong>
+                </div>
+              </div>
+    `
+    cardsMenu.append(card)
   })
+}
+
+
+if (localStorage.getItem('restaurant')) {
+  const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+  console.log(restaurant);
+  changeTitle(restaurant)
+
+  fetch(`./db/${restaurant.products}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItem(data);
+    })
+} else {
+  window.location.href = '/'
+}
+
+
--- js/partners.js
+++ js/partners.js
@@ -1,8 +1,43 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+
 const renderItem = (data) => {
-  data.forEach((dataItem) => {
-    console.log(dataItem);
+  data.forEach((item) => {
+    const a = document.createElement('a')
+    const { image, kitchen, name, price, products, stars, time_of_delivery } = item
+
+    a.setAttribute('href', '/restaurant.html')
+    a.classList.add('card')
+    a.classList.add('card-restaurant')
+    a.dataset.products = products
+
+    console.log(a);
+
+    a.innerHTML = `<img
+                src="${image}"
+                alt="${name}"
+                class="card-image"
+              />
+              <div class="card-text">
+                <div class="card-heading">
+                  <h3 class="card-title">${name}</h3>
+                  <span class="card-tag tag">${time_of_delivery} мин</span>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                  <div class="rating">${stars}</div>
+                  <div class="price">От ${price} Р</div>
+                  <div class="category">${kitchen}</div>
+                </div>
+              </div>
+`
+    a.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      localStorage.setItem('restaurant', JSON.stringify(item))
+      window.location.href = '/restaurant.html'
+    })
+    cardsRestaurants.append(a)
   })
-  console.log(data);
 }
 
 fetch('https://delivery-dd9b5-default-rtdb.europe-west1.firebasedatabase.app/db/partners.json')
@@ -10,3 +45,4 @@ fetch('https://delivery-dd9b5-default-rtdb.europe-west1.firebasedatabase.app/db/
   .then((data) => {
     renderItem(data);
   })
+
